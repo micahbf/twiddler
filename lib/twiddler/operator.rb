95bea416f88c06417f7cd2562fd214bd7c727f0f
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2015-08-29 13:17:32 -0700

    Operator

---
+++ lib/twiddler/operator.rb
@@ -0,0 +1,59 @@
+module Twiddler
+  class Operator
+    class InvalidOperation < StandardError; end
+
+    def operate(operation, midi_event)
+      value = midi_event[:value]
+
+      result = case operation[:op]
+      when :raw
+        value
+      when :lin
+        linear(operation, value)
+      when :log
+        logarithm(operation, value)
+      when :exp
+        exponent(operation, value)
+      else
+        raise InvalidOperation, "invalid operation: #{operation[:op]}"
+      end
+
+      if operation[:integer]
+        result.round
+      elsif round = operation[:round]
+        result.round(round)
+      else
+        result
+      end
+    end
+
+    private
+
+    def linear(operation, value)
+      min, max = operation.values_at(:min, :max)
+      unless min && max
+        raise InvalidOperation, "linear operation needs min and max"
+      end
+
+      slope = (max - min) / 127.0
+      slope * value + min
+    end
+
+    def logarithm(operation, value)
+      coefficient, constant = operation.values_at(:coefficient, :constant)
+      coefficient ||= 1
+      constant    ||= 0
+
+      coefficient * Math.log(value) + constant
+    end
+
+    def exponent(operation, value)
+      base, coefficient, constant = operation.values_at(:base, :coefficient, :constant)
+      base        ||= Math::E
+      coefficient ||= 1
+      constant    ||= 0
+
+      coefficient * base ** value + constant
+    end
+  end
+end
