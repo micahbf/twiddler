9c4f68c421a9475dd78bb0e44716efc88463d1c3
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2015-08-31 10:25:05 -0700

    Basic HUD

---
+++ lib/twiddler/hud.rb
@@ -0,0 +1,16 @@
+require 'tty-table'
+
+module Twiddler
+  class HUD
+    CLEAR = "\e[H\e[2J"
+
+    def update(state)
+      table = TTY::Table.new(header: %w(parameter MIDI value))
+      state.each do |name, data|
+        table << [name, data[:raw], data[:value]]
+      end
+      puts CLEAR
+      table.render(:unicode)
+    end
+  end
+end
