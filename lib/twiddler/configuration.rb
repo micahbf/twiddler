b888add389bc1a09f8279e4cfcadc1ecb10b0863
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2015-09-02 13:20:05 -0700

    Add opt parser

--- lib/twiddler/configuration.rb
+++ lib/twiddler/configuration.rb
@@ -1,19 +1,47 @@
 require 'yaml'
 require 'hashie'
+require 'slop'
 
 module Twiddler
   class Configuration
-    def self.load(yaml_path)
-      @config_path = yaml_path
-      @config = Hashie::Mash.load(yaml_path)
-    end
+    class << self
+      def load(opts)
+        @config_path = opts.delete(:config)
+        yaml_opts = YAML.load_file(@config_path)
+        @config = Hashie::Mash.new(yaml_opts.merge(opts))
+      end
 
-    def self.config
-      @config
-    end
+      def parse_opts
+        Slop.parse do |o|
+          o.string '-c', '--config', 'configuration file'
+          o.string '-t', '--template', 'template file'
+          o.string '-o', '--outfile', 'rendered output file'
+          o.integer '-d', '--debounce', 'debounce time in msec'
+          o.string '-m', '--midi', 'MIDI device name'
+          o.on '-l', '--ls-midi', 'list available MIDI devices' do
+            Twiddler::MIDIDevice.ls
+            exit
+          end
+          o.on '-h', '--help', 'display this help' do
+            puts o
+            exit
+          end
+        end.to_hash
+      end
 
-    def self.expand_path(path)
-      File.expand_path(path, @config_path)
+      def parse_and_load
+        opts = parse_opts
+        raise "must specify config file" unless options[:config]
+        load(opts)
+      end
+
+      def config
+        @config
+      end
+
+      def expand_path(path)
+        File.expand_path(path, @config_path)
+      end
     end
   end
 end
